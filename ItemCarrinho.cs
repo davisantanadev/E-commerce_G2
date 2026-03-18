567fa2178f9dfe3caf6b61ebf8897803941eb9d8
KaikeYoshioka <kaike.yoshioka1703@gmail.com>
2026-03-18 20:23:21 -0300

    Classe ItemCarrinho criada

--- ItemCarrinho.cs
+++ ItemCarrinho.cs
@@ -5,8 +5,35 @@ using System.Threading.Tasks;
 
 namespace E_commerce_G2
 {
-    public class ItemCarrinho
+   public class ItemCarrinho
+{
+    private Produto Produto;
+    public int Quantidade { get; private set; }
+
+    public ItemCarrinho(Produto produto, int quantidade)
     {
-        
+        Produto = produto;
+        Quantidade = quantidade;
     }
+
+    public void AtualizarQuantidade(int novaQuantidade)
+    {   if (novaQuantidade > 0)
+        Quantidade = novaQuantidade;
+
+        else
+        {
+            throw new ArgumentException("A quantidade deve ser maior que zero.");
+        }
+    }
+
+    public Produto GetProduto()
+    {
+        return Produto;
+    }
+
+    public double Subtotal()
+    {
+        return Produto.PrecoComDesconto() * Quantidade;
+    }
+}
 }
