e8132133b24ce55d726a51541a2bf5a3961d0a3d
André Henrique <andre.henri2004@gmail.com>
2026-03-19 11:26:36 -0300

    Imprementação da Classe Produto + UML

--- Produto.cs
+++ Produto.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace E_commerce_G2
+
+        public string Nome { get; set; }
+        public double PrecoBase { get; set; }
+        public int Estoque { get; private set; }
+        private double DescontoPercentual { get; set; }
 {
     public class Produto
     {
-        
+        Nome = nome;
+        PrecoBase = precoBase;
+        Estoque = estoque;
+        DescontoPercentual = 0;
+    }
+
+    public void AplicarDesconto(double percentual)
+    {
+        DescontoPercentual = percentual;
+    }
+
+    public void AjustarEstoque(int quantidade)
+    {
+        Estoque += quantidade;
+    }
+
+    public double PrecoComDesconto()
+    {
+        return PrecoBase - (PrecoBase * (DescontoPercentual / 100));
     }
 }
