c680745769d5c51f3fc393ed0236f8451c3eab12
joaowerner-dev <SEU_EMAIL_DO_GITHUB>
2026-03-18 23:27:17 -0300

    Adiciona lógica do Carrinho: adicionar, remover itens e calcular total com desconto

--- Carrinho.cs
+++ Carrinho.cs
@@ -7,6 +7,45 @@ namespace E_commerce_G2
 {
     public class Carrinho
     {
-        
+        private List<ItemCarrinho> itens = new List<ItemCarrinho>();
+
+        public void AdicionarItem(Produto produto, int quantidade)
+        {
+            if (produto == null || quantidade <= 0)
+                return;
+
+            var itemExistente = itens.Find(i => i.GetProduto() == produto);
+
+            if (itemExistente != null)
+            {
+                itemExistente.AtualizarQuantidade(itemExistente.Quantidade + quantidade);
+            }
+            else
+            {
+                itens.Add(new ItemCarrinho(produto, quantidade));
+            }
+        }
+
+        public void RemoverItem(Produto produto)
+        {
+            var item = itens.Find(i => i.GetProduto() == produto);
+
+            if (item != null)
+            {
+                itens.Remove(item);
+            }
+        }
+
+        public double CalcularTotal()
+        {
+            double total = 0;
+
+            foreach (var item in itens)
+            {
+                total += item.Subtotal();
+            }
+
+            return total;
+        }
     }
 }
